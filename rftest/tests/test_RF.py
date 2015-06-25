10cdddd25a1b836bda79dfb71105a31689c902fd
vagrant <vagrant@precise64.(none)>
2015-06-25 08:10:41 +0000

    added tests for rfserver and rfproxy

--- rftest/tests/test_RF.py
+++ rftest/tests/test_RF.py
@@ -40,11 +40,9 @@ class RF_Tests(object):
         pass
 
 
-#class TestOVS(Tests_RF):
-#class TestOVS(RF_Tests):
 class TestOVS():
 
-    logger = logging.getLogger('Entering TestOVS')
+    logger = logging.getLogger('TestOVS')
 
     def test_netwokvsctl(self,capfd):
         self.logger.info('TestOVS Network ovs-vsctl')
@@ -108,6 +106,40 @@ class TestOVS():
     def verify_tests():
         pass
 
+class Testrfserver():
+
+    logger = logging.getLogger('TestRFServer')
+
+    def test_processrfserverrunning(self,capfd):
+        self.logger.info('Test for process rfserver running')
+        subprocess.call('ps aux| grep rfserver', shell = True)
+        out,err = capfd.readouterr()
+        str2 = 'python ./rfserver/rfserver/py'
+        with open("testout.txt","a") as txtfile:
+             txtfile.write("\n ==== Test for process rfserver running ==== \n ")
+             txtfile.write(out)
+        with open("testerr.txt","a") as txtfile:
+             txtfile.write("\n ==== Test for process rfserver running ==== \n ")
+             txtfile.write(err)
+        assert out.find(str2) != -1
+
+class Testrfproxy():
+
+    logger = logging.getLogger('TestRFProxy')
+
+    def test_processrfproxy(self,capfd):
+        self.logger.info('Test for process rfproxy running')
+        subprocess.call('ps aux| grep rfproxy', shell = True)
+        out,err = capfd.readouterr()
+        str2 = 'ryu-manager --use-stderr --ofp-tcp-listen-port=$CONTROLLER_PORT ryu-rfproxy/rfproxy.py'
+        with open("testout.txt","a") as txtfile:
+             txtfile.write("\n ==== Test for process rfproxy running ==== \n ")
+             txtfile.write(out)
+        with open("testerr.txt","a") as txtfile:
+             txtfile.write("\n ==== Test for process rfproxy running ==== \n ")
+             txtfile.write(err)
+        assert out.find(str2) != -1
+
 
 if __name__ == '__main__':
     description = 'RFTest suite, to run the tests and determine the state of system'
@@ -128,5 +160,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     tests = ['lxc','ovs']
-    server = RF_Run_Tests(args.mongoport, args.controllerport1, args.controllerport2, args.lxcname,tests)
+    server = RF_Tests(args.mongoport, args.controllerport1, args.controllerport2, args.lxcname,tests)
 
