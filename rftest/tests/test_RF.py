00172295780ea67841fec4d8d3f18c1f5f2c7e6d
vagrant <vagrant@precise64.(none)>
2015-06-25 07:23:50 +0000

    ovs tests ready with logging and output files

--- rftest/tests/test_RF.py
+++ rftest/tests/test_RF.py
@@ -1,25 +1,132 @@
 import sys
-import logging
 import time
 import argparse
 #import unittest
 import os
 import pytest
+import logging
+import subprocess
 
-from utils import DumpToFile
+#from utils import DumpToFile
 
-class TestRF():
+logging.basicConfig(
+    filename = 'RFtest.log',
+    level=logging.INFO,
+    format='%(asctime)s %(name)-15s %(levelname)-8s %(message)s',
+    datefmt='%b %d %H:%M:%S'
+    )
 
-    """
-    defnition to check status of openVswitch process.
-    :test_OVS_Status: calls the function execute and passes
-        1. command
-        2. path
-        3. log file name
-        4. log file type
-    """
-    def test_OVS_Status(self):
-        ins = DumpToFile()
-        ins.execute(['ps', 'aux', '|', 'grep', 'ovs'], os.getcwd() , "ovsStatus","txt")
-        ins.execute(['ps', 'aux'], os.getcwd() , "allStatus", "txt")
+
+
+class RF_Tests(object):
+
+    def __init__(self, mongoPort, controllerOne, controllerTwo, lxc, testsToRun):
+        logger = logging.getLogger('RFtest')
+        self.logger.info("test case execution start")
+        self.mongoPort  = mongoPort
+        self.controllerOne = controllerOne
+        self.controllerTwo = controllerTwo
+        self.lxc = lxc
+        self.testsToRun = testsToRun
+
+    def create_parser(self):
+        logger.info("create Parser base function")
+        pass
+
+    def run_tests():
+        pass
+
+    def verify_tests():
+        pass
+
+
+#class TestOVS(Tests_RF):
+#class TestOVS(RF_Tests):
+class TestOVS():
+
+    logger = logging.getLogger('Entering TestOVS')
+
+    def test_netwokvsctl(self,capfd):
+        self.logger.info('TestOVS Network ovs-vsctl')
+        subprocess.call('ovs-vsctl show | grep dp0', shell = True)
+        out,err = capfd.readouterr()
+        with open("testout.txt","a") as txtfile:
+             txtfile.write("\n ==== TestOVS Network ovs-vsctl ==== \n ")
+             txtfile.write(out)
+        with open("testerr.txt","a") as txtfile:
+             txtfile.write("\n ==== TestOVS Network ovs-vsctl ==== \n ")
+             txtfile.write(err)
+        assert out.find("dp0") != -1
+
+    def test_netwokdpctl(self,capfd):
+        self.logger.info('TestOVS Network ovs-dpctl')
+        subprocess.call('ovs-dpctl show | grep dp0', shell = True)
+        out,err = capfd.readouterr()
+        with open("testout.txt","a") as txtfile:
+             txtfile.write("\n ==== TestOVS Network ovs-dpctl ==== \n ")
+             txtfile.write(out)
+        with open("testerr.txt","a") as txtfile:
+             txtfile.write("\n ==== TestOVS Network ovs-dpctl ==== \n ")
+             txtfile.write(err)
+        assert out.find("dp0") != -1
+
+
+    def test_processdbserver(self,capfd):
+        self.logger.info('TestOVS Process ovsdb server')
+        subprocess.call('ps aux | grep ovs', shell = True)
+        out,err = capfd.readouterr()
+        with open("testout.txt","a") as txtfile:
+             txtfile.write("\n ==== TestOVS Process ovsdb server === \n ")
+             txtfile.write(out)
+        with open("testerr.txt","a") as txtfile:
+             txtfile.write("\n ==== TestOVS Process ovsdb server === \n ")
+             txtfile.write(err)
+        assert out.find("ovsdb-server") != -1
+
+    def test_processswitchd(self,capfd):
+        self.logger.info('TestOVS Process ovs-vswitchd')
+        subprocess.call('ps aux | grep ovs', shell = True)
+        out,err = capfd.readouterr()
+        with open("testout.txt","a") as txtfile:
+             txtfile.write("\n ==== TestOVS Process ovs-vswitchd === \n ")
+             txtfile.write(out)
+        with open("testerr.txt","a") as txtfile:
+             txtfile.write("\n ==== TestOVS Process ovs-vswitchd === \n ")
+             txtfile.write(err)
+        assert out.find("ovs-vswitchd") != -1
+
+    def test_specific(self,capfd):
+        self.logger.info('TestOVS Specific')
+        #pass
+
+    def run_tests():
+        self.logger.info('TestOVS testcases start execution')
+        #self.test_netwok()
+        #self.test_process()
+        #self.test_specific()
+
+    def verify_tests():
+        pass
+
+
+if __name__ == '__main__':
+    description = 'RFTest suite, to run the tests and determine the state of system'
+    epilog = 'Report bugs to: https://github.com/routeflow/RouteFlow/issues'
+
+    #config = os.path.dirname(os.path.realpath(__file__)) + "/config.csv"
+    #islconf = os.path.dirname(os.path.realpath(__file__)) + "/islconf.csv"
+
+    parser = argparse.ArgumentParser(description=description, epilog=epilog)
+    parser.add_argument('-m', '--mongoport', default=27017, type = int,
+                        help='port number to verify running of mongodb')
+    parser.add_argument('-c', '--controllerport1', default=6533, type = int,
+                        help='rfproxy controller port1')
+    parser.add_argument('-cc', '--controllerport2', default=6653, type = int,
+                        help='rfproxy controller port2')
+    parser.add_argument('-l', '--lxcname', default='',
+                        help='Lxc container name, should be given to verify, default not supported')
+
+    args = parser.parse_args()
+    tests = ['lxc','ovs']
+    server = RF_Run_Tests(args.mongoport, args.controllerport1, args.controllerport2, args.lxcname,tests)
 
--- rftest/tests/utils.py
+++ rftest/tests/utils.py
@@ -17,34 +17,36 @@ class DumpToFile(object):
 	filetype : to store output in the file format specified.
 
     """
-    def execute(self,command,filepath,filename,filetype):
-        popen = subprocess.Popen(command, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        lines_iterator = iter(popen.stdout.readline, b"")
+    #def execute(self,command,filepath,filename,filetype):
+    def execute(self,command):
+        subprocess.call(command, shell = True) #Popen(command, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        #popen = subprocess.Popen(command, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        #lines_iterator = iter(popen.stdout.readline, b"")
         #lines_iterator_error = iter(popen.stderr.readline, c"")
 
-        if filetype == "json":
-            dumpfile = filepath+"/output/" + filename + ".output" + ".json"
-            try:
-                os.remove(dumpfile)
-            except OSError:
-                pass
-            with open(dumpfile, "a") as jsonfile:
-                for line in lines_iterator:
-                    json.dump(line,jsonfile)
-        elif filetype == "txt":
-            dumpfile = filepath+"/output/" + filename + ".output" + ".txt"
-            try:
-                os.remove(dumpfile)
-            except OSError:
-                pass
-            with open(dumpfile, "a") as txtfile:
-                for line in lines_iterator:
-                    txtfile.write(line)
+       # if filetype == "json":
+       #     dumpfile = filepath+"/output/" + filename + ".output" + ".json"
+       #     try:
+       #         os.remove(dumpfile)
+       #     except OSError:
+       #         pass
+       #     with open(dumpfile, "a") as jsonfile:
+       #         for line in lines_iterator:
+       #             json.dump(line,jsonfile)
+       # elif filetype == "txt":
+       #     dumpfile = filepath+"/output/" + filename + ".output" + ".txt"
+       #     try:
+       #         os.remove(dumpfile)
+       #     except OSError:
+       #         pass
+       #     with open(dumpfile, "a") as txtfile:
+       #         for line in lines_iterator:
+       #             txtfile.write(line)
 
 
-       # dumpfile1 = filepath+"/" + filename + ".error"
-       # try:
-       #     os.remove(dumpfile1)
+       ## dumpfile1 = filepath+"/" + filename + ".error"
+       ## try:
+       ##     os.remove(dumpfile1)
        # except OSError:
        #     pass
        # with open(dumpfile1, "a") as jsonfile:
